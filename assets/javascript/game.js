5bc675092f9dda3efe86d23e971f48cd771afe65
Alex Sato <as4628@columbia.edu>
2018-10-15 22:05:32 -0400

    Worked on character selection:
    	When player chooses a character, it disables all character
    	buttons.
    
    	Also, it adds a highlight for the selected character.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,44 @@
 
 
-var playerChar; 
+var playerChar;
 
-function Character(attack, counter, health){
+function Character(attack, counter, health) {
     this.attack = attack;
     this.counter = counter;
     this.health = health;
-}
+};
 
-var player1 = new Character(100,100,100);
-var player2 = new Character(50,50,50);
-var player3 = new Character(25,25,25);
+var player1 = new Character(100, 100, 100);
+var player2 = new Character(50, 50, 50);
+var player3 = new Character(25, 25, 25);
+$(document).ready(function () {
+    
+    
+    
+    $(".char").on("click",function(){
+        $(this).addClass("highlight");
+        $("#log").text(this.id + " selected.");
+        if(this.id=="strong"){
+            
+            playerChar = player1;
+        }
+        else if(this.id == "medium"){        
+            playerChar = player2;
+        }
+        else{
+            playerChar = player3;
+        }
+        $(".char").css("highlight");
+        $(".char").attr("disabled","disabled");
 
-$(".char").on("click",function(){
-    alert("test");
-    $("#log").text(this.id + " selected.");
-    if(this.id=="strong")
-        playerChar = player1;
-    else if(this.id == "medium")        
-        playerChar = player2;
-    else
-        playerChar = player3;
-        
+        // $(".char").each(function(){
+        //     this.attr("disabled","disabled");
+        // });
+
+    });
+
+    $(".mob").on("click",function(){
+        $("#log").append(this.id+" selected.");
+    })
+
 });
